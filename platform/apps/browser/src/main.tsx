a72e6ca09e955c455bb1c8075a09f2da62bf65bb
Dino Scheidt <github@dino-scheidt.de>
2023-03-03 19:47:59 +0100

    ✏️ Fix views vs `view` folder structure typo

--- platform/apps/browser/src/main.tsx
+++ platform/apps/browser/src/main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-import Router from "@view/router";
+import Router from "@views/router";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
