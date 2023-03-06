8f549e20eb9de2da7eb4d5d4c7e6c76994289a68
Dino Scheidt <github@dino-scheidt.de>
2023-03-06 06:09:55 +0100

    💩 Remove inner ui library pnpm workspace reference, to prevent storybook recursively sourcing symlinked workspace components.
    Added note to .storybook/main.ts
    
    Related to Issues:
    https://github.com/storybookjs/storybook/issues/15913
    https://github.com/storybookjs/storybook/issues/11181

--- platform/_tooling/storybook/.storybook/main.ts
+++ platform/_tooling/storybook/.storybook/main.ts
@@ -3,13 +3,13 @@ import type { StorybookConfig } from "@storybook/react-vite";
 const config: StorybookConfig = {
   stories: [
     "../stories/**/*.mdx",
-    "../stories/**/*.stories.@(js|jsx|ts|tsx)",
-    "../stories/**/*.story.@(js|jsx|ts|tsx)",
+    "../stories/**/*.@(stories|story).@(js|jsx|ts|tsx)",
+
+    // DEPISSUE Storybook PNPM Compatibility: This can lead to recursive story sourcing from symlinked packages within the node_modules folder. Alternative is to provide the list of files directly since adding "!(/node_modules)" to the glob doesn't work See here: https://github.com/storybookjs/storybook/issues/11181#issuecomment-1455449562
 
     // UI Workspace
     "../node_modules/ui/**/*.mdx)",
-    "../node_modules/ui/**/*.stories.@(js|jsx|ts|tsx)",
-    "../node_modules/ui/**/*.story.@(js|jsx|ts|tsx)",
+    "../node_modules/ui/**/*.@(stories|story).@(js|jsx|ts|tsx)",
   ],
   addons: [
     "@storybook/addon-links",
