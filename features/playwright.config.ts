b22834ace50185a97ffddff0ea43ff345a48b657
Dino Scheidt <github@dino-scheidt.de>
2023-04-12 20:53:21 +0200

    👷‍♂️ Be able to run ci related instances in parallel to dev instances. These should run on ##1# port. Add to playwright config #8

--- features/playwright.config.ts
+++ features/playwright.config.ts
@@ -6,6 +6,8 @@ import { defineConfig, devices } from "@playwright/test";
  */
 // require('dotenv').config();
 
+const CI_BROWSER_URL = "http://127.0.0.1:3010";
+
 /**
  * See https://playwright.dev/docs/test-configuration.
  */
@@ -24,7 +26,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: "http://127.0.0.1:3000",
+    baseURL: CI_BROWSER_URL,
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
@@ -70,8 +72,8 @@ export default defineConfig({
 
   // Run your local dev server before starting the tests */
   webServer: {
-    command: "cd ../platform && pnpm run start:browser",
-    url: "http://127.0.0.1:3000",
+    command: "cd ../platform && pnpm run start:browser:ci",
+    url: CI_BROWSER_URL,
     reuseExistingServer: !process.env.CI,
   },
 });
